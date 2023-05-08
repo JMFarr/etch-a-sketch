2922be189a6cb677ec9a511996949948deecce27
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2023-05-07 17:45:46 -0700

    Allow user to set squares per side

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const grid = document.querySelector('#grid');
+const button = document.querySelector('#set-squares');
+
+button.addEventListener('click', () => setSquaresPerSide());
 
 function drawGrid(squaresPerSide) {
     const squareDimensions = Math.floor(grid.offsetWidth / squaresPerSide);
@@ -21,6 +24,14 @@ function deleteGrid() {
     while(grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
+}
+
+function setSquaresPerSide() {
+    let squaresPerSide = prompt("How many squares per side? (Max: 100)");
+    if(squaresPerSide > 100) squaresPerSide = 100;
+
+    deleteGrid();
+    drawGrid(squaresPerSide);
 }
 
 drawGrid(16);
